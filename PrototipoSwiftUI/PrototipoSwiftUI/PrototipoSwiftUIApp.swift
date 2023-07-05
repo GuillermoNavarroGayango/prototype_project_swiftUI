2f3aae70a58d5d7173d83d1f3f035bad6650a22e
Guillermo <guillermo.navarrogayango@plexus.es>
2023-07-05 11:18:19 +0200

    Inicio de proyecto

---
+++ PrototipoSwiftUI/PrototipoSwiftUI/PrototipoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototipoSwiftUIApp.swift
+//  PrototipoSwiftUI
+//
+//  Created by Guillermo Navarro Gayango on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PrototipoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
