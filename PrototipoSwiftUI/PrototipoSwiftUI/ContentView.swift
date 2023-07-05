b8ddcc4d8d16c93c240d4c64a54de018d4c99bf3
Guillermo <guillermo.navarrogayango@plexus.es>
2023-07-05 12:09:54 +0200

    Uso de ZStack

--- PrototipoSwiftUI/PrototipoSwiftUI/ContentView.swift
+++ PrototipoSwiftUI/PrototipoSwiftUI/ContentView.swift
@@ -9,33 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            VStack {
-                Text("Siguiendo")
-                    .font(.largeTitle)
-                    .bold()
-            }
-            Text("Canales recomendados")
-                .foregroundColor(.gray)
-            HStack {
-                Rectangle()
-                    .foregroundColor(.blue)
-                    .frame(width: 118, height: 68)
-                VStack(alignment: .leading)  {
-                    HStack {
-                        Circle()
-                            .frame(width: 18, height: 18)
-                            .foregroundColor(.blue)
-                        Text("username")
-                            .font(.headline)
-                    }
-                    Text("streaming de programación...")
-                        .foregroundColor(.gray)
-                    Text("Solo hablando")
-                        .foregroundColor(.gray)
+        ZStack {
+            VStack(alignment: .leading, spacing: 8) {
+                VStack {
+                    Text("Siguiendo")
+                        .font(.largeTitle)
+                        .bold()
                 }
-
+                Text("Canales recomendados")
+                    .foregroundColor(.gray)
+                HStack {
+                    Rectangle()
+                        .foregroundColor(.blue)
+                        .frame(width: 118, height: 68)
+                    VStack(alignment: .leading)  {
+                        HStack {
+                            Circle()
+                                .frame(width: 18, height: 18)
+                                .foregroundColor(.blue)
+                            Text("username")
+                                .font(.headline)
+                        }
+                        Text("streaming de programación...")
+                            .foregroundColor(.gray)
+                        Text("Solo hablando")
+                            .foregroundColor(.gray)
+                    }
+                }
             }
+            Text("Suscribiros a SwiftBeta")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.red)
+                .underline()
+                .background(.black)
+                .rotationEffect(.degrees(-20))
         }
     }
 }
